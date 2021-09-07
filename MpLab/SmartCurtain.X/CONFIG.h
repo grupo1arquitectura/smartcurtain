355ce809072147b9a4b06ef5130a4f947b67ae40
Oscar Fernando Hoyos Carvajal <osfehoyos@unicauca.edu.co>
2021-09-07 07:32:37 -0500

    Creacion de proyecto MPlab (Main y Config)

---
+++ MpLab/SmartCurtain.X/CONFIG.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   CONFIG.h
+ * Author: Oscar Hoyos
+ *
+ * Created on 7 de septiembre de 2021, 07:24 AM
+ */
+
+#ifndef CONFIG_H
+#define	CONFIG_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* CONFIG_H */
+
---
+++ MpLab/SmartCurtain.X/Main.c
@@ -0,0 +1,13 @@
+/*
+ * File:   Main.c
+ * Author: Oscar Hoyos
+ *
+ * Created on 7 de septiembre de 2021, 07:27 AM
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
